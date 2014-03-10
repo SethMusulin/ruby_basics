dc158ff61351ad205937794cb602a92349cefb51
your-name <your-email>
2014-03-10 10:19:14 -0600

    Commit of tests

--- bin/roman_numerals.rb
+++ bin/roman_numerals.rb
@@ -4,7 +4,7 @@ puts " Give me an arabic number to convert "
 this_number = gets.chomp.to_i
 roman_numerals = {1 => "I", 4 => "IV", 5 => "V", 9 => "IX", 10 => "X", 40 => "XL", 50 => "L", 90 => "XC", 100 => "C", 400 => "CD", 500 => "D", 900 => "CM", 1000 => "M"}
 output = ""
-current_key = 1
+current_key = self
 
 
 while this_number > 0
